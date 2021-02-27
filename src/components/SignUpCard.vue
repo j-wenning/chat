18a5a94b1363e608caca316fe7afdbc372994c7e
j-wenning <wenning.justin@gmail.com>
2021-02-27 12:41:57 -0800

    Updated sign up card component to use single quotes for svg

--- src/components/SignUpCard.vue
+++ src/components/SignUpCard.vue
@@ -36,8 +36,8 @@
         autocomplete='current-password'
         required />
       <label for='password' class='flex text-sm'>
-        <svg class='h-5 w-5 text-blue-500 mr-1' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        <svg class='h-5 w-5 text-blue-500 mr-1' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+          <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
         </svg>
         <span class='text-gray-900 dark:text-gray-100'>Passwords must be between 8 and 32 characters.</span>
       </label>
