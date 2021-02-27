dc18de5ae76e9d266f47d3c118ffb0a069e54314
j-wenning <wenning.justin@gmail.com>
2021-02-27 12:36:24 -0800

    Created sign in view

---
+++ src/views/SignIn.vue
@@ -0,0 +1,14 @@
+<template>
+<div class='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-800 py-12 px-4 sm:px-6 lg:px-8'>
+  <SignInCard />
+</div>
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+import SignInCard from '@/components/SignInCard.vue'
+
+@Options({ name: 'SignIn', components: { SignInCard } })
+
+export default class SignIn extends Vue {}
+</script>
