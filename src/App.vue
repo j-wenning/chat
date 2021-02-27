60b522efe38e879ad746402ee3925f5e7540cf2c
j-wenning <wenning.justin@gmail.com>
2021-02-27 13:00:02 -0800

    Updated app src to remove initial skeleton and add dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <button @click='toggleDarkMode' role='switch' :aria-checked='dark' class='fixed p-3 m-2 right-0'>
+    <span class='sr-only'>Toggle dark mode</span>
+    <svg v-if='dark' class='h-7 w-7 text-gray-100' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+    </svg>
+    <svg v-else class='h-7 w-7 text-gray-900' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+      <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z' />
+    </svg>
+  </button>
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { Options, Vue } from 'vue-class-component'
 
-#nav {
-  padding: 30px;
-}
+const htmlClasses = document.querySelector('html')?.classList
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@Options({
+  data () { return { dark: false } },
+  methods: { toggleDarkMode () { this.dark = !this.dark } },
+  mounted () { this.dark = htmlClasses.contains('dark') },
+  watch: { dark () { htmlClasses.toggle('dark') } }
+})
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default class App extends Vue {}
+</script>
