8d9c01f41fb6939712c85133c1f81bbb6e685754
j-wenning <wenning.justin@gmail.com>
2021-02-27 13:50:53 -0800

    Updated app and views moving responsibility  of background color styling to app

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,5 @@
 <template>
-<div class='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-800 py-12 px-4 sm:px-6 lg:px-8'>
+<div class='min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
   <SignUpCard />
 </div>
 </template>
