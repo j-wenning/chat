bcfb394c42b8ce0be70aca5e46b97468fb898b37
j-wenning <wenning.justin@gmail.com>
2021-02-28 11:49:41 -0800

    Created forgot view

---
+++ src/views/Forgot.vue
@@ -0,0 +1,14 @@
+<template>
+<div class='min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+  <ForgotCard />
+</div>
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+import ForgotCard from '@/components/ForgotCard.vue'
+
+@Options({ name: 'Forgot', components: { ForgotCard } })
+
+export default class Forgot extends Vue {}
+</script>
