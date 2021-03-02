9de31a51bf6c6a08d90551e3807df3fdefd23047
j-wenning <wenning.justin@gmail.com>
2021-03-01 20:37:42 -0800

    Updated tailwind config adding more theme and variant extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: 'class', // false, 'media', or 'class'
   theme: {
-    extend: {}
+    extend: {
+      maxWidth: {
+        '3xs': '8rem',
+        '2xs': '14rem'
+      }
+    }
   },
   variants: {
     extend: {
-      cursor: ['disabled'],
       backgroundColor: ['disabled'],
-      opacity: ['disabled']
+      cursor: ['disabled'],
+      opacity: ['disabled'],
+      overflow: ['hover'],
+      padding: ['hover'],
+      rotate: ['group-hover']
     }
   },
   plugins: [
