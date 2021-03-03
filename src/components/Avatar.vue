91c0f18055bd4315b65f7f8c38037fb33910b062
j-wenning <wenning.justin@gmail.com>
2021-03-02 19:33:37 -0800

    Created avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,34 @@
+<template>
+<img
+  :src='_src'
+  :alt='_alt'
+  class='h-10 w-10 rounded-full object-contain' />
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      required: false
+    },
+    alt: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    _src () { return this.src || 'https://via.placeholder.com/150' },
+    _alt () { return this.alt || '' }
+  }
+})
+
+export default class Avatar extends Vue {}
+</script>
+
+<style>
+
+</style>
