5a5d18ed8f057d1a56b7adf91dc4d150c1d5341e
j-wenning <wenning.justin@gmail.com>
2021-03-02 19:36:11 -0800

    Created profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,41 @@
+<template>
+<div class='max-w-2xs w-full p-3 rounded-sm text-center button-select bg-gray-400 dark:bg-gray-900'>
+  <div class='relative flex flex-wrap justify-center'>
+    <Avatar class='h-24 w-24' :src='avatar' />
+    <p class='w-full break-words mt-3'>{{name}}</p>
+    <router-link
+      ref='link'
+      @blur='handleBlur'
+      class='button mt-3'
+      :to='"/chat/" + id'>Message</router-link>
+  </div>
+</div>
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+import Avatar from './Avatar.vue'
+
+@Options({
+  name: 'Profile',
+  components: { Avatar },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    handleBlur: {
+      type: Function,
+      required: true
+    }
+  },
+  updated () { this.$refs.link.$el.focus() },
+  computed: {
+    avatar () { return this.user.avatar || '' },
+    name () { return this.user.name || '' },
+    id () { return this.user.id || '' }
+  }
+})
+
+export default class Profile extends Vue {}
+</script>
