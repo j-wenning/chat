92a6f8c3b86d76ef31de42ec3177c76669769e1b
j-wenning <wenning.justin@gmail.com>
2021-03-04 15:30:09 -0800

    Updated store index to include users in state Updated index to include temporary randomized user list

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,24 @@
 import { createStore } from 'vuex'
+import { randInt } from '@/scripts/random.ts'
+
+interface User {
+  id: number;
+  avatar: string;
+  name: string;
+}
+
+interface State {
+  users: User[];
+}
+
+const fakeString = (minLength: number, maxLength: number): string =>
+  [...Array(randInt(minLength, maxLength))].map(() => String.fromCharCode(randInt(32, 126))).join('')
+
+const fakeUsers = [...Array(35)].map((_, id) => ({ id, avatar: '', name: fakeString(2, 36) }))
 
 export default createStore({
   state: {
+    users: fakeUsers
   },
   mutations: {
   },
@@ -10,6 +27,7 @@ export default createStore({
     toggleUsers: (): void => undefined
   },
   getters: {
+    users: (state: State): Array<User> => state.users
   },
   modules: {
   }
