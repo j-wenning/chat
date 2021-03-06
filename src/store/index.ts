f2be4d1c12746d207742c20a3a5575626dbcb16f
j-wenning <wenning.justin@gmail.com>
2021-03-05 20:40:07 -0800

    Updated store index to include is medium toggled state and associated mutations, actions, and getters

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,11 +9,13 @@ interface User {
 
 interface State {
   users: User[];
+  isMediumToggled: boolean;
   isUsersToggled: boolean;
   isSettingsToggled: boolean;
 }
 
 enum Mutations {
+  setMedium = 'setMedium',
   toggleUsers = 'toggleUsers',
   toggleSettings = 'toggleSettings'
 }
@@ -26,20 +28,24 @@ const fakeUsers = [...Array(35)].map((_, id) => ({ id, avatar: '', name: fakeStr
 export default createStore({
   state: {
     users: fakeUsers,
+    isMediumToggled: false,
     isUsersToggled: false,
     isSettingsToggled: false
   },
   mutations: {
+    setMedium: (state: State, payload: boolean): void => { state.isMediumToggled = payload },
     toggleUsers: (state: State): void => { state.isUsersToggled = !state.isUsersToggled },
     toggleSettings: (state: State): void => { state.isSettingsToggled = !state.isSettingsToggled }
   },
   actions: {
     updateFormInput: (): void => undefined,
+    setMedium: ({ commit }, payload: boolean): void => commit(Mutations.setMedium, payload),
     toggleUsers: ({ commit }): void => commit(Mutations.toggleUsers),
     toggleSettings: ({ commit }): void => commit(Mutations.toggleSettings)
   },
   getters: {
     users: (state: State): Array<User> => state.users,
+    isMediumToggled: (state: State): boolean => state.isMediumToggled,
     isUsersToggled: (state: State): boolean => state.isUsersToggled,
     isSettingsToggled: (state: State): boolean => state.isSettingsToggled
   },
