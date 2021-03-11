8d001c28aa29e93c396c092d0eb8ef03f610db9a
j-wenning <wenning.justin@gmail.com>
2021-03-11 12:54:05 -0800

    Updated server index to utilize db and exit handler modules Updated index to be wrapped in async block

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
-require('dotenv/config')
-const env = require('./parse-env.ts')(process.env)
-const { PORT: port } = env
-const express = require('express')
-const app = express()
+(async () => {
+  require('dotenv/config')
+  const env = require('./parse-env')(process.env)
+  const { PORT: port } = env
+  const exitHandler = require('./exit-handler')
+  const app = require('express')()
+  const db = await require('./db')(env).catch((err: any): void => { console.error(err) })
+  const { cursorTo } = require('readline')
+  const { stdout } = process
 
-app.use(require('./session.ts')(env))
+  app.use(require('./session')(env))
 
-app.use((_: any, res: any) => res.send('Hello (:'))
+  app.use((_: any, res: any): void => { res.send('Hello (:') })
 
-app.listen(port, () => console.log('Listening on port ' + port))
+  app.listen(port, (): void => { stdout.write(`Listening on port ${port}.\n`) })
+
+  exitHandler((): void => {
+    cursorTo(process.stdout, 0)
+    stdout.write(`Application terminated.\nPort ${port} freed.\n`)
+  })
+})()
