8f776c8b03b3298bfeebf469cf0a6982893ba088
j-wenning <wenning.justin@gmail.com>
2021-03-13 12:10:35 -0800

    Updated session module to reflect env changes

--- server/session.ts
+++ server/session.ts
@@ -1,17 +1,18 @@
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
 
-export default (env: any): any => {
-  const {
-    SESSION_SECRET: sessionSecret,
-    SESSION_EXPIRY: sessionExpiry,
-    DB_URL: dbURL
-  } = env
-  return session({
-    resave: true,
-    saveUninitialized: false,
-    secret: sessionSecret,
-    store: MongoStore.create({ mongoUrl: dbURL, collectionName: 'sessions' }),
-    cookie: { maxAge: sessionExpiry, sameSite: true }
-  })
-}
+const {
+  SESSION_SECRET: sessionSecret,
+  SESSION_EXPIRY: sessionExpiry,
+  DB_URL: dbURL
+} = process.env as any
+
+const sessionExport = session({
+  resave: true,
+  saveUninitialized: false,
+  secret: sessionSecret,
+  store: MongoStore.create({ mongoUrl: dbURL, collectionName: 'sessions' }),
+  cookie: { maxAge: sessionExpiry, sameSite: true }
+})
+
+export default sessionExport
