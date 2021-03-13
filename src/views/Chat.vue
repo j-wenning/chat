f0a900404b6ee84286e545da5d835054de1aabb6
j-wenning <wenning.justin@gmail.com>
2021-03-12 16:43:08 -0800

    Created chat view

---
+++ src/views/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+<div class='min-h-screen w-full flex'>
+  <ChatSidebar />
+  <ChatHistory />
+  <ChatSettings />
+</div>
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+import ChatSidebar from '@/components/ChatSidebar.vue'
+import ChatHistory from '@/components/ChatHistory.vue'
+import ChatSettings from '@/components/ChatSettings.vue'
+
+@Options({ name: 'Chat', components: { ChatSidebar, ChatHistory, ChatSettings } })
+
+export default class App extends Vue {}
+</script>
